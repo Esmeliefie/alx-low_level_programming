9f3376d9e953958e0ed0fbd7ec9e9ebd3908a86a
Esmeralda Kok <esykok@gmail.com>
2022-09-08 01:06:21 -0700

    quick betty fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
+/**
+ * main - prints the sentence "Programming is like building a multilingual puzzle
+ * Return: 0 if exited properly
+ */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
