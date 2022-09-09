d8ba3fcf0404efc534365f896bea5acb0f8028f9
Esmeralda Kok <esykok@gmail.com>
2022-09-09 05:21:39 -0700

    betty

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -3,21 +3,20 @@
 /**
 *main - prints alphabet letter in lowercase followed by upercase letters
 *followed by a new line
-*return: 0 always
+*Return: 0 always
 */
-
 int main(void)
 {
-    char ch;
+	char ch;
 
-    for (ch = 'a'; ch <= 'z'; ch++)
-    {
-        putchar(ch);
-    }
-    for (ch = 'A'; ch <= 'Z'; ch++)
-    {
-        putchar(ch);
-    }
-    putchar('\n');
-    return (0);
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	for (ch = 'A'; ch <= 'Z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
 }
